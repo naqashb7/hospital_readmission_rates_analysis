cbc223f67a6691cf35d02fd2d303aa75e29d93d2
naqashb7 <naqash.bhatti7@gmail.com>
2026-03-24 19:15:50 +0000

    App complete

--- pages/6_Ethical_Considerations.py
+++ pages/6_Ethical_Considerations.py
@@ -9,158 +9,63 @@ and deployed responsibly.
             
 ---
             
-# Background 
+# GDPR
 
-Diabetes patients being readmitted to hospital has been a big strain on healthcare systems. It can be very expensive so readmitting a patient can cost lots of money. 
-By being able to predict why a patient is being readmitted, we can figure out ways to prevent readmission and tackle the issue at source.
-
+The General Data Protection Regulation (GDPR) is a comprehensive European Union law enacted on May 25, 2018, that governs how organizations collect, use, store, and protect the personal data of individuals within the EU. 
+It focuses on privacy rights, transparency, and data accountability, giving individuals greater control over their personal information.
             
-The dataset being used records patient encounters in hospital. It looks at what occurred when they were in hospital, how long they stayed, what changes happened to their medications,
-what was their background and what did historical hospital visitation look like.
-
-
 
 ---
 
-## Dataset background
-- **Source:** [Diabetes 130 US hospitals for years 1999-2008 Dataset](https://www.kaggle.com/datasets/brandao/diabetes) from Kaggle
-- **Records:** ~100,000 records
-- **Target Variable:** Readmitted (0 = Not readmitted or Readmitted More than 30 days after discharge, 1 = Readmitted less than 30 days after discharge))
-- **Other Variables:**
-    - Age
-    - Gender 
-    - Race
-    - Weight
-    - Admission type
-    - Number of lab and non lab procedures
-    - Primary, Secondary & Tertiary  Diagnoses
-    - Blood Sugar testing
-    - Medicinal Changes
+## Bias
+            
+* Automation Bias – The data is US data and from the original analysis and visualisations, indicate that 4 days was the mean stay length for patients who were readmitted to hospital. In the US, healthcare works via an insurance system. 
+            It may be that the longer stays required longer care, however, patients may not have been able to afford the care which led to discharge. 
+            Early discharge may cause patients to not receive full care and hence cause patients to return to hospital. 
+            This indicates health inequalities within the economic system, something that is not considered by my model as it is not a feature of the dataset. 
+            Automation bias needs to be considered here as an ethical issue as over reliance on the model could lead to ignorance around other factors impacting readmittance. 
+* Algorithmic Bias – Algorithmic bias is bias in the model due to human prejudices often caused by skewed, unrepresentative or flawed data. 
+            The use of age, gender and race from this dataset can cause algorithmic bias as the data may have been flawed. 
+            The model will then create its own assumptions and understanding from that data creating a bias.
+* Historic data – This data is historical data from between 1999-2008. This means that the data is almost outdated by 20 years.
+            There has been a lot of medical and economic evolution since then. 
+            The outcomes from this data and the predictive analytics of the model may not be relevant today as the clinical practices and economic structures will have changed since then.
+
 
 ---
             
-## Key Findings
-- In the model, Age was given the 4th greatest weight as a predictor of readmittance. However, through analysis, age had a strong impact on readmittance.
-- African-Americans were seen to be more likely to be readmitted to hospital and also spent more time in hospital.
-- Individuals with medical histories that had higher numbers of Emergency visits in the year preceding the hospital encounter, were more likely to be readmitted to hospital.
+## Privacy & GDPR
+Data minimisation is a core privacy principle within GDPR requirements. 
+The US does not have a similar framework and so this data does not follow those requirements. 
+To follow those requirements, the following columns were removed and were identified as either sensitive health, economic or unique identifiable information:
+
+* Weight – sensitive health data. <mark style='background-color: #FF6B6B; color: white;'>*REMOVED*</mark>
+* Payer code – sensitive health and economic information <mark style='background-color: #FF6B6B; color: white;'>*REMOVED*</mark>
+* Encounter ID – sensitive identifiable information <mark style='background-color: #FF6B6B; color: white;'>*REMOVED*</mark>
+* Patient Number - sensitive identifiable information <mark style='background-color: #FF6B6B; color: white;'>*REMOVED*</mark>
+* Admission Type ID - sensitive identifiable information <mark style='background-color: #FF6B6B; color: white;'>*REMOVED*</mark>
+* Admission Source ID - sensitive identifiable information <mark style='background-color: #FF6B6B; color: white;'>*REMOVED*</mark>
+
+This allows me to still be able to build an effective predictive model while reducing the risk of patients being identified from the data.
+
+
 
 ---            
-
-## Full Project Process
-1. ETL pipeline
-2. Exploratory Data Analysis (EDA)
-3. Machine Learning Pipeline
-4. Predictive App and dashboard design and deployment
-
----            
-## Hypotheses
-            
-1) Age: 
-    * H<sub>0</sub>: Age has no impact on diabetes patient readmittance rates to hospital.
-2) Race:
-    * H<sub>0</sub>: Race has no impact on diabetes patient readmittance rates to hospital.
-3) Gender:
-    * H<sub>0</sub>: Gender has no impact on diabetes patient readmittance rates to hospital.
-4) Time in hospital:
-    * H<sub>0</sub>: Time spent in hospital during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-5) Number of medications:
-    * H<sub>0</sub>: Number of medications administered during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-6) Number of Lab procedures:
-    * H<sub>0</sub>: Number of lab procedures performed during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-7) Number of Non-lab procedures:
-    * H<sub>0</sub>: Number of non-lab procedures performed during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-8) Prior Visit History:
-    * H<sub>0</sub>: The type of prior visits a patient does (Inpatient, Outpatient, Emergency) over the last year, has no impact on diabetes patient readmittance rates to hospital.
-9) Diagnosis type:
-    * H<sub>0</sub>: The type of diagnoses during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-10) Medicinal Changes:
-    * H<sub>0</sub>: Any Medicinal changes during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-11) Testing performed:
-    * H<sub>0</sub>: Blood testing performed during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-
+## Fairness & Social Impact
             
 
----
 
-## Conclusions
+In the data, some records show that some patients had more visits than others. 
+This initially indicates that patients needed to keep coming back to hospital for more treatment. 
+In reality, this may not have been the case as it may have been that the patients did not have access to the adequate healthcare for their treatment in the first place. 
+This should be further investigated, especially as the data is from across 130 different US hospitals and so the quality of healthcare would not necessarily have been universal. 
 
-
-1) Age: 
-    * H<sub>0</sub>: Age has no impact on diabetes patient readmittance rates to hospital.
-        * The analysis above shows that as a patient gets older, they tend to spend more time in hospital and are more likely to be readmitted.
-        * The Null hypothesis can be rejected.
-        * The reasoning for this could be due to weaker immune systems in older patients.
-2) Race:
-    * H<sub>0</sub>: Race has no impact on diabetes patient readmittance rates to hospital.
-        * The analysis shows that African-American patients spent the most time in hospital while Asian patients spent the least time.
-        * Asian patients were also the least readmitted to hospital.
-        * There is a clear discrepancy seen in the data amongst races.
-        * This means that the null hypothesis can be rejected.
-3) Gender:
-    * H<sub>0</sub>: Gender has no impact on diabetes patient readmittance rates to hospital.
-        * There was no clear significant difference in readmittance rates between males and females.
-        * The null hypothesis can not be rejected.
-4) Time in hospital:
-    * H<sub>0</sub>: Time spent in hospital during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * From the heatmap analysis, it is clear that the longer a patient had spent in hospital, the more likely they were to be readmitted, whether it was less than or more than 30 days later.
-        * It is also seen that patients who were readmitted to hospital had a mean stay of 4 days while those who did not had a mean stay of 3 days.
-        * Considering both previous analyses, the null hypothesis can be rejected.
-5) Number of medications:
-    * H<sub>0</sub>: Number of medications administered during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * There was a slight higher number of medications administered to patients that were readmitted to the hospital over those who were not.
-        * However, there was large variablity in the data and a lot of outliers so the data is not reliable.
-        * For this reason, the null hypothesis can not be rejected.
-6) Number of Lab procedures:
-    * H<sub>0</sub>: Number of lab procedures performed during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * There was no significant difference in readmittance vs no readmittance when looking at the number of lab procedures the patient had gone through in their previous encounter.
-        * Additional to this, there were lots of outliers in the data which indicated high variability and so low reliability.
-        * Considering this, the null hypothesis can not be rejected.
-7) Number of Non-lab procedures:
-    * H<sub>0</sub>: Number of non-lab procedures performed during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * There was no significant difference between readmitted patients and non-readmitted patients when looking at the number of non-lab procedures performed.
-        * The null hypothesis can not be rejected.
-8) Prior Visit History:
-    * H<sub>0</sub>: The type of prior visits a patient does (Inpatient, Outpatient, Emergency) over the last year, has no impact on diabetes patient readmittance rates to hospital.
-        * The type of prior visits a patient had in the year preceding the hospital encounter had a direct impact on the readmittance likelihood of the patient.
-        * Emergency visits indicated the highest patient readmittance rate at 64%.
-        * Emergency visits also indicated the highest patient readmittance rate for both Less than 30 days and more than 30 days.
-        * The null hypothesis can be rejected.
-9) Diagnosis type:
-    * H<sub>0</sub>: The type of diagnoses during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * When Diabetes was the primary diagnosis it had the highest readmittance rates.
-        * Genitourinary diagnoses were consistently amongst the highest for readmittance rates in secondary and tertiary diagnoses.
-        * This showed that different diagnoses had differing impacts on patient readmittance and so showed a connection.
-        * The null hypothesis can be rejected.
-10) Medicinal Changes:
-    * H<sub>0</sub>: Any Medicinal changes during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * There was a 7% increase in patients who were prescribed diabetes medication being readmitted to hospital over those who were not.
-        * This indicated that there may have been errors in the prescribing of patients which then meant patients would be readmitted to hospital.
-        * Further to this, the deep analysis of different diabetes medication showed:
-            * Metformin and Insulin were the most commonly prescribed medications.
-            * Metformin experienced no changes amongst patients, but Insulin did.
-            * Where there were changes made to Insulin dosages, readmittance rates were lower.
-            * Where Insulin was either not prescribed or the dosage was kept steady, readmittance rates were higher.
-        * Based off of this, the null hypothesis will be rejected.
-11) Testing performed:
-    * H<sub>0</sub>: Blood testing performed during a patient's latest encounter, has no impact on diabetes patient readmittance rates to hospital.
-        * There were two tests performed:
-            * Max Glucose Serum
-                * In patients where Measurement>300, readmittance rates were highest.
-                * In patients where no test was performed, readmittance was higher than the normal measurement but only by 1%
-            * HbA1C testing
-                * In patients where Measurement>8, readmittance rates were highest.
-                * However, the same rates were seen for patients where no test was performed.
-        * Based off of this analysis, the null hypothesis can be rejected.
-
----
-## Machine Learning Summary
-2 ML models were tested to see which would work best in predicting readmission rates:
-
-    -Logistic Regression
-    -Random Forest
-
-The Logistic Regression model was selected as the most suitable model 
-for patient readmission prediction, achieving a recall of 0.50 for stroke cases and an accuracy of 0.66.            
+            
+The dataset indicates that African American patients had to be readmitted more often than any of the other ethnicities. 
+Based off of this model, it would indicate that race was likely to cause readmittance. 
+However, in reality, it could be that health inequality was the actual cause and in more dense African American areas, lower quality healthcare was leading to readmittance of patients. 
+The aim with AI should be that outcomes lead to a fair solution and is not more beneficial to one group of people over another. This is why fairness is important to considered.
+        
             
             
 """, unsafe_allow_html=True)
